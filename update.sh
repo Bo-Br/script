812d245c2d64842844673ba6737c416d696b2244
Bogdane <bogdanbraun4@gmail.com>
2026-04-24 12:45:53 +0200

    Refactor package installation commands in update.sh

--- update.sh
+++ update.sh
@@ -3,9 +3,13 @@ apt-get update
 apt-get install
 apt-get upgrade
 apt-get dist-upgrade
-apt-get install git curl npm nvm n -y
+apt-get install git -y
+apt-get install curl -y
+apt-get install npm -y
+apt-get install nvm -y
+apt-get install n -y
 curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash -
-apt-get install -y nodejs
+apt-get install nodejs -y 
 curl -o /usr/local/bin/n https://raw.githubusercontent.com/visionmedia/n/master/bin/n
 chmod +x /usr/local/bin/n
 n stable
