b85b5a1a0d7cc01c1e383043f9d933acc728223c
Bogdane <bogdanbraun4@gmail.com>
2026-04-24 12:43:46 +0200

    Modify update.sh for Node.js 21.x installation
    Updated Node.js installation script to use version 21.x and removed conflicting nodejs removal commands.

--- update.sh
+++ update.sh
@@ -3,18 +3,16 @@ apt-get update
 apt-get install
 apt-get upgrade
 apt-get dist-upgrade
-apt-get install git curl nodejs npm nvm n -y
-apt remove nodejs
-apt autoremove
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-apt install -y nodejs
+apt-get install git curl npm nvm n -y
+curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash -
+apt-get install -y nodejs
 curl -o /usr/local/bin/n https://raw.githubusercontent.com/visionmedia/n/master/bin/n
 chmod +x /usr/local/bin/n
 n stable
 node -v
 cd /var/www/
 git clone https://github.com/Bo-Br/backend-test.git
-cd backend-test
+cd /var/www/backend-test
 npm install
 npm run build
 ip addr
